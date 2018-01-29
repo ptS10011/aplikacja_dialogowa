343fd04364563a0e2ef33cde54d212e64b3603c4
ptS10011 <ptS10011@users.noreply.github.com>
2018-01-29 20:28:23 +0100

    Obsluga bazy

---
+++ MySQL_Test/MySQL_Test/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace MySQL_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dbCon = new DBConnect();
+            dbCon.InsertLot("warszawa", "londyn", "styczen");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
